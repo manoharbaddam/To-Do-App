f6652f3c09dddd7a4509344f5ed0186c9ab81f82
Baddam Manohar Reddy <manoharbaddam789@gmail.com>
2026-02-23 15:30:49 +0530

    First Commit

---
+++ backend/apps/users/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager,PermissionsMixin,AbstractBaseUser
+import uuid
+# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self,email,password,**extra_fields):
+        if not email :
+            raise ValueError("Email must be provided")
+        email = self.normalize_email(email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,email,password,**extra_fields):
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_superuser",True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+        
+        return self.create_user(email,password,**extra_fields)
+        
+
+class User(AbstractBaseUser,PermissionsMixin):
+    uuid = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100,blank=True,null=True)
+    last_name = models.CharField(max_length=100,blank=True,null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff  = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['first_name']
+
+    def __str__(self):
+        return f"{self.email}"
